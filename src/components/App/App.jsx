71c5548e32d5fb3d7eaf392c4aa6979f8af19d41
Piotr Adamski <113289205+PiotrAdamski8888@users.noreply.github.com>
2023-07-02 13:00:16 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,18 +6,21 @@ import { ContactList } from 'components/Contact/ContactList';
 import css from './App.module.css';
 
 export function App() {
-  const [contacts, setContacts] = useState(() => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
     const storedContacts = localStorage.getItem('contacts');
-    return storedContacts
-      ? JSON.parse(storedContacts)
-      : [
-          { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-          { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-          { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-          { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ];
-  });
-  const [filter, setFilter] = useState('');
+    if (storedContacts) {
+      setContacts(JSON.parse(storedContacts));
+    } else {
+      setContacts([
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]);
+    }
+  }, []);
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
